4985cff8b8bda26f0788aa48d47be399af6613db
khangzxrr <khangzxrr@gmail.com>
2023-02-13 22:35:47 +0700

    keep only tournament create

--- bird-reactJs-main/src/views/client/Login.jsx
+++
@@ -1,9 +0,0 @@
-const Login = (props)=>{
-    return (
-        <div className="row">
-            <p>Login</p>
-        </div>
-    )
-}
-
-export default Login;
--- bird-reactJs-main/src/views/client/Register.jsx
+++
@@ -1,9 +0,0 @@
-const Register = (props)=>{
-    return (
-        <div className="row">
-            <p>Register</p>
-        </div>
-    )
-}
-
-export default Register;
